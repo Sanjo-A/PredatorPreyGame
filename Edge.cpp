02419783c5659eaea485b4f921855570aa898d58
litarrow <50309848+litarrow@users.noreply.github.com>
2019-05-06 07:58:15 -0700

    First draft Edge.cpp (Sae Hyoung Oh)

---
+++ Edge.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * Program Name: Group Project
+ * Author: Sae Hyoung Oh
+ * Date: 5/6/2019
+ * Description:
+ * This is the implement file for the Edge class. It details the methods.
+ *****************************************************************************/
+#include "Edge.hpp"
+
+/******************************************************************************
+Name:		Edge()
+Called by:	
+Calls:		N/A
+Passed:		N/A
+Returns:	N/A
+Description:
+This is the default constructor for Ant class. The type is set for '*'.
+******************************************************************************/
+Edge::Edge()
+{
+	type = '*';
+}
