8a2d8073041e4bbab95f75f5c5adc2908c05e713
wtayjay <42885732+wtayjay@users.noreply.github.com>
2019-05-09 16:36:34 -0700

    Create Menu.hpp

---
+++ Menu.hpp
@@ -0,0 +1,19 @@
+/******************************************
+*May 9, 2019
+*Group 24
+*Header file for menu class
+*******************************************/
+
+#include <iostream>
+#ifndef MENU_HPP
+#define MENU_HPP
+
+class menu()
+{
+  private:
+  
+  public:
+    int getSteps();
+};
+
+#endif
