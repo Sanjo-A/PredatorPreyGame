5d0189f9a46e2b3c501edc7ddc7549fe3faa9891
wtayjay <42885732+wtayjay@users.noreply.github.com>
2019-05-09 15:34:22 -0700

    Update Game.hpp

---
+++ Game.hpp
@@ -0,0 +1,17 @@
+/******************************************************
+*May 9th, 2019
+*Group 24
+*Header file for Game class
+*******************************************************
+
+#include "doodlebug.hpp"
+#include <iostream>
+
+class Game
+{
+  Private:
+  
+  
+  Public:
+    void gameflow(int steps);
+}
