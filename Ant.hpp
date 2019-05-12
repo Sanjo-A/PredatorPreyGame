1af8ba31c89766c970a8a8751637e30148cf09e0
litarrow <50309848+litarrow@users.noreply.github.com>
2019-05-12 11:09:57 -0700

    working version

--- Ant.hpp
+++ Ant.hpp
@@ -4,7 +4,7 @@
  * Date: 5/6/2019
  * Description:
  * This is the header file for the Ant class. It is a derived class of Critter
- * class. It overrides the virtual classes. 
+ * class. It overrides the virtual classes.
  *****************************************************************************/
 #ifndef ANT_HPP
 #define ANT_HPP
@@ -13,63 +13,63 @@
 class Ant : public Critter
 {
 public:
-/******************************************************************************
-Name:		Ant()
-Called by:	
-Calls:		N/A
-Passed:		N/A
-Returns:	N/A
-Description:
-This is the default constructor for Ant class. The type is set for 'O', and
-breedCount for 3.
-******************************************************************************/
+	/******************************************************************************
+	Name:		Ant()
+	Called by:
+	Calls:		N/A
+	Passed:		N/A
+	Returns:	N/A
+	Description:
+	This is the default constructor for Ant class. The type is set for 'O', and
+	breedCount for 3.
+	******************************************************************************/
 	Ant();
 
-/******************************************************************************
-Name:		Ant()
-Called by:	
-Calls:		N/A
-Passed:		const Ant &rhs
-Returns:	N/A
-Description:
-This is the copy constructor for Ant class. The copies the type and breedCount
-from the right-hand-side Critter to instantiate an Ant object.
-******************************************************************************/
-	Ant(const Ant &rhs);
+	/******************************************************************************
+	Name:		Ant()
+	Called by:
+	Calls:		N/A
+	Passed:		const Ant &rhs
+	Returns:	N/A
+	Description:
+	This is the copy constructor for Ant class. The copies the type and breedCount
+	from the right-hand-side Critter to instantiate an Ant object.
+	******************************************************************************/
+	Ant(const Ant* &rhs);
 
-/******************************************************************************
-Name:		move()
-Called by:	
-Calls:		getType() from Critter class, intGen() from RNG class
-Passed:		N/A
-Returns:	char
-Description:
-This moves the Ant in a random direction, as specified in the assignment.
-It returns the character for the direction it moves.
-******************************************************************************/
+	/******************************************************************************
+	Name:		move()
+	Called by:
+	Calls:		getType() from Critter class, intGen() from RNG class
+	Passed:		N/A
+	Returns:	char
+	Description:
+	This moves the Ant in a random direction, as specified in the assignment.
+	It returns the character for the direction it moves.
+	******************************************************************************/
 	virtual char move();
 
-/******************************************************************************
-Name:		breed()
-Called by:	
-Calls:		getType() from Critter class, intGen() from RNG class
-Passed:		N/A
-Returns:	char
-Description:
-This breeds the Ant in a random direction, as specified in the assignment.
-It returns the character for the direction it breeds.
-******************************************************************************/
+	/******************************************************************************
+	Name:		breed()
+	Called by:
+	Calls:		getType() from Critter class, intGen() from RNG class
+	Passed:		N/A
+	Returns:	char
+	Description:
+	This breeds the Ant in a random direction, as specified in the assignment.
+	It returns the character for the direction it breeds.
+	******************************************************************************/
 	virtual char breed();
 
-/******************************************************************************
-Name:		age()
-Called by:	
-Calls:		breed(), move() from Ant class
-Passed:		N/A
-Returns:	void
-Description:
-This calls decreases the breedCount by one.
-******************************************************************************/
+	/******************************************************************************
+	Name:		age()
+	Called by:
+	Calls:		breed(), move() from Ant class
+	Passed:		N/A
+	Returns:	void
+	Description:
+	This calls decreases the breedCount by one.
+	******************************************************************************/
 	virtual void age();
 };
 #endif
