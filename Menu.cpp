409f1c130e196925c462361f5ea89b6d3882aa44
wtayjay <42885732+wtayjay@users.noreply.github.com>
2019-05-12 11:40:47 -0700

    comments and whitespace

--- Menu.cpp
+++ Menu.cpp
@@ -15,7 +15,7 @@ int Menu::getSteps()
   cout << "How many steps would you like to run?" << endl;
   cin >> steps;
   
-  while (!cin)
+  while (!cin) //will run until user enters an int
   {
     cout << "That is not an integer. Please try again." << endl;
     cin.clear();
@@ -23,7 +23,7 @@ int Menu::getSteps()
     cin >> steps;
   }
   
-  while (steps < 1 && steps > 5000)
+  while (steps < 1 && steps > 5000) //will run until user enters int in the specified range
   {
     cout << "Please enter an integer between 1 and 5000." << endl;
     cin.clear();
@@ -31,14 +31,16 @@ int Menu::getSteps()
     cin >> steps;
   }
   
+  cout << "Please hit enter between each step." << endl;
   return steps;
 }
 
-int Menu::playAgain()
+int Menu::playAgain() //function to see if user wants to run again
 {
   int choice = 0;
   cout << "Would you like to play again? Please enter 1 for yes and 2 for no." << endl;
   cin >> choice;
+ 
   while (!cin)
   {
     cout << "That is not a valid choice. Please try again." << endl;
@@ -46,7 +48,8 @@ int Menu::playAgain()
     cin.ignore();
     cin >> choice;
   }
-  while (choice != 1 && choice != 2)
+ 
+  while (choice != 1 && choice != 2) //only lets user enter 1 or 2 for choice
   {
     cout << "That is not a valid choice. Please try again." << endl;
     cin.clear();
