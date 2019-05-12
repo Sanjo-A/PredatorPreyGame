594a07565297518bef378b322e9d15aa8892a4b5
litarrow <50309848+litarrow@users.noreply.github.com>
2019-05-12 01:21:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,8 +20,8 @@ using std::endl;
 
 //function prototypes
 char read(Critter *obj);
-void setGrid(Critter*** grid, int size);
-void initializeBoard(Critter*** grid, int size);
+void setGrid(Critter*** &grid, int size);
+void initializeBoard(Critter*** &grid, int size);
 
 /******************************************************************************
 Name:		main()
@@ -35,16 +35,16 @@ Example codes included.
 ******************************************************************************/
 int main()
 {
-int size = 20;		//size of the board
+	int size = 20;		//size of the board
 
-//create a dynamic 2D array for critters
+	//create a dynamic 2D array for critters
 	Critter ***grid = new Critter**[size + 2];	//add 2 for the edges
 
 	for (int i = 0; i < size + 2; i++)
 	{
 		grid[i] = new Critter*[size + 2];		//add 2 for the edges
 	}
-//initiate the Critters with base class (also serves as empty)
+	//initiate the Critters with base class (also serves as empty)
 	for (int i = 1; i < size + 1; i++)
 	{
 		for (int j = 1; j < size + 1; j++)
@@ -52,7 +52,7 @@ int size = 20;		//size of the board
 			grid[i][j] = new Critter;
 		}
 	}
-//initiate the edges with Edge class
+	//initiate the edges with Edge class
 	for (int i = 0; i < size + 2; i++)	//left edge
 	{
 		grid[i][0] = new Edge;
@@ -70,19 +70,18 @@ int size = 20;		//size of the board
 		grid[size + 1][i] = new Edge;
 	}
 
-//assign derived classes to different elements in the array to imitate move()
-	delete grid[1][1];
-	delete grid[1][2];
-	delete grid[5][5];
-	delete grid[10][10];
-	grid[1][1] = new Ant;
-	grid[1][2] = new Doodlebug;
-	grid[5][5] = new Ant;
-	grid[10][10] = new Doodlebug;
+	////assign derived classes to different elements in the array to imitate move()
+	//delete grid[1][1];
+	//delete grid[1][2];
+	//delete grid[5][5];
+	//delete grid[10][10];
+	//grid[1][1] = new Ant;
+	//grid[1][2] = new Doodlebug;
+	//grid[5][5] = new Ant;
+	//grid[10][10] = new Doodlebug;
 
-	initializeBoard(grid, size);
-//define linked-classes-relationship before and/or after each move()
-	
+	//define linked-classes-relationship before and/or after each move()
+
 	Game game;
 	Menu menu;
 	int choice = 1;
@@ -105,7 +104,7 @@ int size = 20;		//size of the board
 		choice = menu.playAgain();
 	} while (choice == 1);
 
-//print the grid
+	//print the grid
 	cout << endl;
 	for (int i = 0; i < size + 2; i++)
 	{
@@ -117,27 +116,27 @@ int size = 20;		//size of the board
 	}
 	cout << endl;
 
-//check if reading worked, using read(). Pass the linked object.
-	//should be 'L'
-	cout << "grid[1][1]: " << read(grid[1][1]) << endl;
-	//should be '*'
-	cout << "grid[0][1]: " << read(grid[1][1]->left) << endl;
-	//should be '*'
-	cout << "grid[1][0]: " << read(grid[1][1]->up) << endl;
-	//should be 'X'
-	cout << "grid[1][2]: " << read(grid[1][1]->right) << endl;
-	//should be ' '
-	cout << "grid[2][1]: " << read(grid[1][1]->down) << endl;
-	//should be 'L'
-	cout << "grid[1][2]->move(): " << grid[1][2]->move() << endl;
-	//should be 'D'
-	cout << "grid[1][1]->move(): " << grid[1][1]->move() << endl;
-	//should be random
-	cout << "grid[5][5]->move(): " << grid[5][5]->move() << endl;
-	//should be random
-	cout << "grid[10][10]->move(): " << grid[10][10]->move() << endl;
+	////check if reading worked, using read(). Pass the linked object.
+	//	//should be 'L'
+	//cout << "grid[1][1]: " << read(grid[1][1]) << endl;
+	////should be '*'
+	//cout << "grid[0][1]: " << read(grid[1][1]->left) << endl;
+	////should be '*'
+	//cout << "grid[1][0]: " << read(grid[1][1]->up) << endl;
+	////should be 'X'
+	//cout << "grid[1][2]: " << read(grid[1][1]->right) << endl;
+	////should be ' '
+	//cout << "grid[2][1]: " << read(grid[1][1]->down) << endl;
+	////should be 'L'
+	//cout << "grid[1][2]->move(): " << grid[1][2]->move() << endl;
+	////should be 'D'
+	//cout << "grid[1][1]->move(): " << grid[1][1]->move() << endl;
+	////should be random
+	//cout << "grid[5][5]->move(): " << grid[5][5]->move() << endl;
+	////should be random
+	//cout << "grid[10][10]->move(): " << grid[10][10]->move() << endl;
 
-//deallocate dynamic array
+	//deallocate dynamic array
 	for (int i = 0; i < size + 2; i++)
 	{
 		delete[] grid[i];
@@ -172,47 +171,47 @@ Passed:		Critter*** grid, int size
 Returns:	void
 Description:
 This sets the linked-class-relationship between the Critter class objects
-inside the array. 
+inside the array.
 ******************************************************************************/
-void setGrid(Critter*** grid, int size)
+void setGrid(Critter*** &grid, int size)
 {
 	for (int i = 0; i < size + 2; i++)
 	{
 		for (int j = 0; j < size + 1; j++)
 		{
 			//set all the "right" relationships, except the rightmost ones
-			grid[i][j]->right = grid[i][j + 1];
+			grid[i][j]->right = grid[i][j + 1]->getType();
 		}
 		for (int j = 1; j < size + 2; j++)
 		{
 			//set all the "left" relationships, except the leftmost ones
-			grid[i][j]->left = grid[i][j - 1];
+			grid[i][j]->left = grid[i][j - 1]->getType();
 		}
 		//set the leftmost relationship
-		grid[i][0]->left = nullptr;
+		grid[i][0]->left = 'N';
 		//set the rightmost relationship
-		grid[i][size + 1]->right = nullptr;
+		grid[i][size + 1]->right = 'N';
 	}
 	for (int j = 0; j < size + 2; j++)
 	{
 		for (int i = 0; i < size + 1; i++)
 		{
 			//set all the "down" relationships, except the downmost ones
-			grid[i][j]->down = grid[i + 1][j];
+			grid[i][j]->down = grid[i + 1][j]->getType();
 		}
 		for (int i = 1; i < size + 2; i++)
 		{
 			//set all the "up" relationships, except the upmost ones
-			grid[i][j]->up = grid[i - 1][j];
+			grid[i][j]->up = grid[i - 1][j]->getType();
 		}
 		//set the upmost relationship
-		grid[0][j]->up = nullptr;
+		grid[0][j]->up = 'N';
 		//set the downmost relationship
-		grid[size + 1][j]->down = nullptr;
+		grid[size + 1][j]->down = 'N';
 	}
 }
 /******************************************************************************
- Name:        initializeBoard()
+ Name:        InitializeBoard()
  Called by:    N/A
  Calls:        N/A
  Passed:        Critter*** grid, int size
