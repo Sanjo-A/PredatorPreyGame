cf276e3c61d5b634de84a8c96d765c893d052bca
Zajano <50506522+Zajano@users.noreply.github.com>
2019-05-12 22:32:27 -0700

    Add files via upload
    changed prompts for clarification

--- Menu.cpp
+++ Menu.cpp
@@ -15,7 +15,7 @@ void Menu::menuNumBugs(int rowSize, int colSize, InputValidation valid)
 
     std::cout << "Enter number of ants in grid [0..." << rowSize*colSize << "]" << endl;
     numAnts=valid.intValidation(0, rowSize*colSize);
-    
+
 
     std::cout << "Enter number of doodlebugs in grid [0..." << (rowSize*colSize - numAnts) << "]" << endl;
     numDoodlebugs=valid.intValidation(0, (rowSize*colSize - numAnts) );
@@ -25,7 +25,8 @@ void Menu::menuNumBugs(int rowSize, int colSize, InputValidation valid)
 int Menu::getSteps(InputValidation valid)
 {
   int steps = 0;
-  cout << "How many steps would you like to run?" << endl;
+  cout << "How many turns would you like to run?" << endl;
+  cout << "Press enter after each turn to continue to the next one." << endl;
     steps = valid.intValidation(1,5000);
   return steps;
 }
@@ -33,8 +34,8 @@ int Menu::getSteps(InputValidation valid)
 int Menu::playAgain(InputValidation valid) //function to see if user wants to run again
 {
   int choice = 0;
-  cout << "Would you like to play again? Please enter 1 for yes and 2 for no." << endl;
-  choice = valid.intValidation(1,2);  
+  cout << "Would you like to continue playing? Please enter 1 for yes and 2 for no." << endl;
+  choice = valid.intValidation(1,2);
   return choice;
 }
 
